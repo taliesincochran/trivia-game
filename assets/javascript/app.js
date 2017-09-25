ff899cbc7c849f596d50f90081b07f42425b70bd
Taliesin <tdcochran2@gmail.com>
2017-09-25 14:41:49 -0400

    redid everything, used ajax

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,115 +1,200 @@
 var game = {
+	"triviaCategories": {
+		"general": {
+			"number": 9,
+			"name": "General Knowledge",
+		},
+		"entBook":{
+			"number": 10,
+			"name": "Books",
+		},
+		"entFilm":{
+			"number": 11,
+			"name": "Film",
+		},
+		"entMovie":{
+			"number": 12,
+			"name": "Music",
+		},
+		"entTh": {
+			"number": 13,
+			"name": "Musicals & Theatres",
+		},
+		"entTV": {
+			"number": 14,
+			"name": "Television",
+		},
+		"entVG":{
+		"number": 15,
+		"name": "Video Games",
+		},
+		"entBG":{
+		"number": 16,
+		"name": "Board Games",
+		},
+		"sciNat":{
+		"number": 17,
+		"name": "Nature",
+		},
+		"sciCom":{
+		"number": 18,
+		"name": "Computers",
+		},
+		"sciMath":{
+		"number": 19,
+		"name": "Mathematics",
+		},
+		"myth":{
+		"number": 20,
+		"name": "Mythology",
+		},
+		"sport":{
+		"number": 21,
+		"name": "Sports",
+		},
+		"geo":{
+		"number": 22,
+		"name": "Geography",
+		},
+		"his":{
+		"number": 23,
+		"name": "History",
+		},
+		"pol":{
+		"number": 24,
+		"name": "Politics",
+		},
+		"art":{
+		"number": 25,
+		"name": "Art",
+		},
+		"cel":{
+		"number": 26,
+		"name": "Celebrities",
+		},
+		"ani":{
+		"number": 27,
+		"name": "Animals",
+		},
+		"veh":{
+		"number": 28,
+		"name": "Vehicles",
+		},
+		"entCB":{
+		"number": 29,
+		"name": "Comics",
+		},
+		"sciGad":{
+		"number": 30,
+		"name": "Gadgets",
+		},
+		"jAM":{
+		"number": 31,
+		"name": "Japanese Anime & Manga",
+		},
+		"carAni":{
+		"number": 32,
+		"name": "Cartoon & Animations",
+		},
+	},
+	"difficultyChoosen":"",
+	"catagoryChoosen":"",
+	"catagoryNumber":0,
+	"queryURL":"",
+	//tracks game progress and number of wins
+	"questionNumber": 0,
+	"winCounter":0,
+	"playerRight":0,
+	"playerWrong":0,
+	"playerSkipped":0,
 
-	"questions": [
-	{
-		"question": "What was the longest seige in history?",
-		"answer1": "The Siege of Ceuta, 1694 AD.", 
-		"answer2":"The Siege of Candia, 1648 AD.", 
-		"answer3":"The Siege of Philidelphia(in Egypt), 1378 AD.", 
-		"answer4":"The Siege of Drepana, 249 BC.",
-		"correctAnswer": "The Siege of Ceuta, 1694 AD.",
-		"correctPicture": "src='assets/images/question0.png'",
-	},
-	{
-		"question": 'How did Xerxes get his army across from Asia Minor to the Greek mainland during the second Persian invasion of Greece (480 BC - 479 BC")?',
-		"answer1": "He marched them around the north shore of the Black Sea.", 
-		"answer2":"He built a pontoon bridge across the Hellespont and march.", 
-		"answer3":"He moved his troops by galley sailing south of Crete and then north to Greece.", 
-		"answer4":"He moved his troops by trireme across the Sea of Marmara.",
-		"correctAnswer": "He built a pontoon bridge across the Hellespont.",
-		"correctPicture": "src='assets/images/question1.png'",
-	},
-	{
-		"question": "Where did the Allies land during the D-day invasion?",
-		"answer1": "Brittany", 
-		"answer2":"The Cotentin Peninsula", 
-		"answer3":"Calais", 
-		"answer4":"Normandy",
-		"correctAnswer": "Normandy",
-		"correctPicture": "src='assets/images/question2.png'",
-	},
-	{
-		"question": "In which city was Archduke Franz Ferdinand of Austria assasinated?",
-		"answer1": "Belgrade", 
-		"answer2":"Vienna", 
-		"answer3":"Serajevo", 
-		"answer4":"Budapest",
-		"correctAnswer": "Serajevo",
-		"correctPicture": "src='assets/images/question3.png'",
-	},
-	{
-		"question": "What was the first battle of the U.S. Civil War?",
-		"answer1": "The Battle of Sewell's Point", 
-		"answer2":"The Battle of Fort Sumter", 
-		"answer3":"The Battle of Hoke's Run", 
-		"answer4":"The Battle of First Manassas",
-		"correctAnswer": "The Battle of Fort Sumter",
-		"correctPicture": "src='assets/images/question4.png'",
-	},
-	{
-		"question": "Which of the following was not invented in China?",
-		"answer1": "Paper making", 
-		"answer2":"Movable Type Printing", 
-		"answer3":"The Rocket", 
-		"answer4":"Chess",
-		"correctAnswer": "Chess",
-		"correctPicture": "src='assets/images/question5.png'",
-	},
-	{
-		"question": "Who first circumnavigated the globe?",
-		"answer1": "James Cook", 
-		"answer2":"Vasco da Gama", 
-		"answer3":"Ibn Battuta", 
-		"answer4":"Ferdinand Magellen",
-		"correctAnswer": "Ferdinand Magellen",
-		"correctPicture": "src='assets/images/question6.png'",
-	},
-	{
-		"question": "Who is credited for discovering Greenland?",
-		"answer1": "Erik Thorvaldsson", 
-		"answer2":"Leif Eriksson", 
-		"answer3":"Olaf Tryggvason", 
-		"answer4":"Flóki Vilgerðarson",
-		"correctAnswer": "",
-		"correctPicture": "src='assets/images/question7.png'",
-	},
-	{
-		"question": "What was the largest Empire of all time?",
-		"answer1": "The British Empire", 
-		"answer2":"The Mongol Empire", 
-		"answer3":"The Roman Empire", 
-		"answer4":"The Russion Empire",
-		"correctAnswer": "The British Empire",
-		"correctPicture": "src='assets/images/question8.png'",
-	},
-	{
-		"question": "When was the year without a summer?",
-		"answer1": "1758 AD", 
-		"answer2":"1365 AD", 
-		"answer3":"1816 AD", 
-		"answer4":"1655 AD",
-		"correctAnswer": "1758 AD",
-		"correctPicture": "src='assets/images/question9.png'",
-	}
-	],
-	"start": function () {
-		$("#startButton").on("click", function (){
-			$("#startPage").addClass("hidden");
-			$("#main").removeClass("hidden");
+	queryURL:"",
+	"chooseCatagory": function() {
+		$(".catagoryBtn").on("click", function() {
+			game.catagoryChoosen=this.id;
+			$("#catagory").addClass("hidden");
+			$("#difficulty").removeClass("hidden");
+			console.log(game.catagoryChoosen);
+			game.catagoryNumber = game.triviaCategories[game.catagoryChoosen].number;
+			game.chooseCatagory();
 		})
 	},
-	"questionNumber": 0,
+	"chooseDifficulty": function() {
+		$(".difficultyBtn").on("click", function() {
+			game.difficultyChoosen = this.id;
+			console.log(this);
+			game.queryURL = "https://opentdb.com/api.php?amount=10&category=" + game.catagoryNumber + "&difficulty=" + game.difficultyChoosen + '&type=multiple'; 
+			console.log(game.queryURL);
+			$("#difficulty").addClass("hidden");
+			$("#main").removeClass("hidden");
+			game.getQuestion();
+		})
+	},	
 	"getQuestion": function() {
-
-		$("#currentQuestion").text(game.questions[game.questionNumber].question);
-		$("#answer1").text(game.questions[game.questionNumber].answer1);
-		$("#answer2").text(game.questions[game.questionNumber].answer2);
-		$("#answer3").text(game.questions[game.questionNumber].answer3);
-		$("#answer4").text(game.questions[game.questionNumber].answer4);
+		$.ajax({
+		url : game.queryURL,
+		method: "GET"
+		}).done(function(response) {
+			$("#currentQuestion").text(response[game.questionNumber].question);
+		//array of possible incorrect answer, then adds correct answer at a random place
+			var possible = [response.incorrect_answer[0],response.incorrect_answer[1],response.incorrect_answer[3]];
+			var correctPlace = Math.floor(Math.random()*4);
+			possible.splice(correctPlace,0,response.correct_answer);
+			console.log(possible);
+			$("#answer0").text(possible[0]);
+			$("#answer1").text(possible[1]);
+			$("#answer2").text(possible[2]);
+			$("#answer3").text(possible[3]);		
+		}
 	},
+	// // "checkAnswer": function () {
+	// // 	$(".answerBtn").on("click", function() {
+	// // 		console.log(this);
+	// // 		console.log(this.id);
+	// // 		if (this.id === game.questions[game.questionNumber].correctAnswer) {
+	// // 			game.playerRight++;
+	// // 			console.log(game.questions[game.questionNumber].correctAnswer);
+
+	// // 			game.winScreen();
+	// // 		} else {
+	// // 			game.playerWrong++;
+	// // 			game.loseScreen();
+	// // 		} 
+	// // 	});
+	// // },
+	// "update": function () {
+	// 	$("#numberRight").text(game.playerRight);
+	// 	$('#numberWrong').text(game.playerWrong);
+	// 	$('#numberSkipped').text(game.playerSkipped);
+	// },
+	// "winScreen": function () {
+	// 	$("#main").addClass('hidden');
+	// 	$("#feedback").removeClass('hidden');
+	// 	$("#correctIncorrect").text("correct");
+	// 	$("#rightAnswer").text(game.questions[game.questionNumber].correctAnswerText);
+	// 	$("#correctPicture").removeClass("hidden").css("src",game.questions[game.questionNumber].correctPicture).css("alt",game.questions[game.questionNumber].altText)
+	// 	game.update();
+	// 	game.playerRight++;
+	// },
+	// "loseScreen": function () {
+	// 	$("correctIncorrect").text("incorrect.");
+	// 	game.playerWrong++;
+	// 	$("#correctPicture").addClass("hidden").css("src","'#'").css("alt","'#'");
+	// 	game.update();
+	// },
+	// "nextQuestion": function () {
+	// 	$("#next").on("click", function() {
+	// 	$("#main").removeClass('hidden');
+	// 	$("#feedback").addClass('hidden');
+	// 	$("#right").empty();
+	// 	$("#rightAnswer").empty();
+	// 	game.questionNumber++;
+	// 	}),
+	// }
+
+
 	
-}
-game.start();
-game.getQuestion();
-	
+
+game.chooseCatagory();
+game.chooseDifficulty();
 
